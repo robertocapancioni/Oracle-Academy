fd3bb8c1b189b69437362ccc436f00be78d5c019
Roberto Capancioni <roberto.capancioni@gmail.com>
2026-03-26 08:58:41 +0100

    Add SQL query to retrieve post timeline data

---
+++ APEX_2026_03_26/Timeline.sql
@@ -0,0 +1,22 @@
+select
+ p.id,
+ p.created_by AS user_name,
+ p.post_comment AS comment_text,
+ p.document,
+ p.document_mimetype,
+
+ apex_util.get_since(p.created) post_date,
+
+ (
+     select count(*) from AM_REACTIONS amr
+     where amr.post_id=p.id
+ ) as REACTIONS,
+
+ (
+     select 'user-has-liked' from AM_REACTIONS amr
+     where amr.post_id=p.id and created_by=:APP_USER
+ ) USER_REACTION_CSS
+
+ from AM_POSTS p
+
+ order by p.created desc
